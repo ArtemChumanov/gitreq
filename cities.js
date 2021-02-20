4a082930968d65235a29800d931c8af6ed5992e0
ArtemChumanov <artem_chumanov@ukr.net>
2021-02-20 21:48:15 +0200

    Add origin

---
+++ cities.js
@@ -0,0 +1,26 @@
+const _=require('lodash');
+const citises=require("./data.json");
+
+// const output=_(citises)
+//     .filter(c=>c.population>451114444)
+//     .countBy(c=>c.country)
+//     .toPairs()
+//     .map(c=>_.zipObject(['country','city'],c))
+//     .orderBy(c=>c.cities,"desc")
+//     .take(5)
+//     .value()
+// console.log(output)
+
+const greaterThan=_.curryRight(_.gte)
+const populationGreaterThen=num=>_.conforms({population:greaterThan(num)})
+const zip=_.curry(_.zipObject)
+const out=_(citises)
+    .filter(populationGreaterThen(500000))
+    .countBy('country')
+    .toPairs()
+    .map(zip(['country','city']))
+    .orderBy("city","desc")
+    .take(6)
+    .value()
+console.log(out)
+
---
+++ hoc.js
@@ -0,0 +1,18 @@
+const _=require('lodash')
+//
+const mult=(a,b)=>a*b;
+ const divide=(a,b)=>a/b;
+// const double=_.partial(mult,2)
+// console.log(double(21))
+// const half=_.partial(divide,_,2)
+// console.log(half(21))
+
+/**********************************/
+
+const curriedDivide=_.curry(divide);
+console.log(curriedDivide(10,2));
+console.log(curriedDivide(10)(2))
+
+const notFlatArray=[1,2,3,[4,5]]
+const sumFlat=_.flow([_.concat,_.flattenDeep,_.sum]);
+console.log(sumFlat(notFlatArray))
