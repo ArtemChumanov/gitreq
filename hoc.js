322cca3118934ab2484f2ecf3e462a35b83a2ced
ArtemChumanov <artem_chumanov@ukr.net>
2021-02-25 10:47:13 +0200

    sdsd

--- hoc.js
+++ hoc.js
@@ -16,3 +16,6 @@ console.log(curriedDivide(10)(2))
 const notFlatArray=[1,2,3,[4,5]]
 const sumFlat=_.flow([_.concat,_.flattenDeep,_.sum]);
 console.log(sumFlat(notFlatArray))
+console.log(1)
+console.log(1)
+console.log(1)
